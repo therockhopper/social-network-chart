6da2acaab7712404e5e5038c93d860ce61248baa
fschmidt <frank.schmidt@redspace.com>
2020-04-14 19:46:53 -0300

    chore - update source csv

--- chart.py
+++ chart.py
@@ -5,22 +5,23 @@ import csv
 def main():
 
     points = []
+    size_dict = {}
 
     with open("source.csv") as f:
         reader = csv.reader(f)
         titles = next(reader)
         titles.pop(0) # first item in CSV headers is always empty
+        size_dict = {el:0 for el in titles}
         for rowIndex, row in enumerate(reader):
             rowTitle = row.pop(0) # remove name of row
             for colIndex, col in enumerate(row):
                 colTitle = titles[colIndex]
-                print(rowTitle, '->', colTitle, ': ', col)
-                colLoopIndex = 0
-                while colLoopIndex < int(col):
+                size_dict[rowTitle] += int(col)
+                if int(col) > 0:
                     points.append((int(rowIndex),int(colIndex)))
-                    colLoopIndex += 1
 
 
+    print(size_dict)
     g = Graph(points)
     g.vs["name"] = titles
     g.vs["label"] = g.vs["name"]
@@ -28,13 +29,15 @@ def main():
     layout = g.layout("kk")
 
     visual_style = {}
-    visual_style["vertex_size"] = 20
+    visual_style["vertex_size"] = [size_dict[name] for name in g.vs["name"]]
     visual_style["vertex_label"] = g.vs["name"]
+    visual_style["vertex_label_dist"] = 1
     visual_style["layout"] = layout
-    visual_style["bbox"] = (300, 300)
-    visual_style["margin"] = 20
+    visual_style["edge_curved"] = 0
+    visual_style["edge_width"] = [(int(size_dict[name]) / 10) for name in g.vs["name"]]
+    visual_style["margin"] = 50
 
-    plot(g, "social_network.svg", **visual_style)
+    plot(g, "social_network.pdf", **visual_style)
 
 if __name__ == "__main__":
     main()
